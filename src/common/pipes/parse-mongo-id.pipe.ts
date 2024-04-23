6e5800633589c8ccd9216096617483b96cc3a136
Greg Perez <ext_ghperezm@falabella.cl>
2024-04-23 17:57:59 -0400

    first commit 03-pokedex

--- src/common/pipes/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -1,8 +1,13 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
 
 @Injectable()
 export class ParseMongoIdPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+  transform(value: string, metadata: ArgumentMetadata) {
+    //console.log({ value, metadata });
+    if (!isValidObjectId(value)) {
+      throw new BadRequestException(`${value} is not a valid MongoID`)
+    }
+    return value.toUpperCase();
   }
 }
